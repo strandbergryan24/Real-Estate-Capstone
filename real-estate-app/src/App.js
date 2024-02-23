9fec781769b34c8a0cf979667901b0bf720b3f18
Ryan Strandberg <strandbergryan@gmail.com>
2024-02-23 11:53:43 -0700

    created best listings

--- real-estate-app/src/App.js
+++ real-estate-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react' 
 import NavBar from './components/Navbar/NavBar';
 import Hero from './components/Hero/Hero';
+import Best from './components/Best/Best'
 
 function App() {
   return (
     <div>
       <NavBar />
       <Hero />
+      <Best />
     </div>
   );
 }
--- real-estate-app/src/components/Hero/Hero.js
+++ real-estate-app/src/components/Hero/Hero.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import {AiOutlineSearch} from 'react-icons/ai'
 import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero'> 
-        <div className='content'>
-            <h1>Find the Perfect Home</h1>
-            <p>Discover your dream home with us. Whether you're searching for a cozy apartment in the heart of the city, a spacious family house in the suburbs, or a luxurious waterfront property, we're here to help you find the perfect place to call home</p>
-        </div>
-        <div>
+    <div className='hero'>
+      <div className='content'>
+        <h1>Find the Perfect Home</h1>
+        <p className='search-text'>Discover your dream home with us. Whether you're searching for a cozy apartment in the heart of the city, a spacious family house in the suburbs, or a luxurious waterfront property, we're here to help you find the perfect place to call home</p>
+        <form className='search'>
+          <div>
+            <input type='text' placeholder='Enter Keyword' />
+          </div>
+          <div className='radio'>
+            <input type='radio' checked />
+            <label>Buy</label>
+            <input type='radio' />
+            <label>Rent</label>
+            <button type='submit'><AiOutlineSearch className='icon' /></button>
+          </div>
+        </form>
+      </div>
+      <div>
 
-        </div>
+      </div>
     </div>
   )
 }
