644664e9513b239650003f802fcbe870aa7fc6e4
Ryan Strandberg <strandbergryan@gmail.com>
2024-02-22 17:01:09 -0700

    Created Header Element

--- real-estate-app/src/App.js
+++ real-estate-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react' 
-import NavBar from './components/NavBar';
+import NavBar from './components/Navbar/NavBar';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 }
---
+++ real-estate-app/src/components/Hero/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'> 
+        <div className='content'>
+            <h1>Find the Perfect Home</h1>
+            <p>Discover your dream home with us. Whether you're searching for a cozy apartment in the heart of the city, a spacious family house in the suburbs, or a luxurious waterfront property, we're here to help you find the perfect place to call home</p>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
