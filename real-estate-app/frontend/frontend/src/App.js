e2972d5a940502e2abcd8d50e065884f8ae1c566
Ryan Strandberg <strandbergryan@gmail.com>
2024-02-26 11:22:05 -0700

    front end recovered

--- real-estate-app/frontend/frontend/src/App.js
+++ real-estate-app/frontend/frontend/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react' 
-import Featured from './componenets/Featured/Featured'
 import NavBar from './components/Navbar/NavBar';
 import Hero from './components/Hero/Hero';
-import Best from './components/Best/Best';
 
 function App() {
   return (
     <div>
       <NavBar />
       <Hero />
-      <Best />
-      <Featured />
     </div>
   );
 }
