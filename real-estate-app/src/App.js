1620b61bbb7043c55daac8c8226a2ea5798060c7
Ryan Strandberg <strandbergryan@gmail.com>
2024-02-25 23:03:21 -0700

    Frontend Compnents and Style Round 1 Done

--- real-estate-app/src/App.js
+++ real-estate-app/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react' 
-import Featured from './componenets/Featured/Featured'
+import Featured from './components/Featured/Featured'
 import NavBar from './components/Navbar/NavBar';
 import Hero from './components/Hero/Hero';
-import Best from './components/Best/Best';
 
 function App() {
   return (
     <div>
       <NavBar />
       <Hero />
-      <Best />
       <Featured />
     </div>
   );
--- real-estate-app/src/components/Featured/Featured.js
+++ real-estate-app/src/components/Featured/Featured.js
@@ -6,42 +6,41 @@ import './Featured.css'
 
 
 const Featured = () => {
-  return (
-    <div className='featured'>
-        <h1 className='featured-text'>Top featured Listings</h1>
-        <p>Selected listing by City, State, & Zip based on views</p>
-        <div className='container'>
-            <img src={Apt1} alt='' />
-            <img src={Apt2} alt='' />
-            <img src={Apt3} alt='' />
-            <img src={Apt3} alt='' />
-            <img src={Apt2} alt='' />
-            <div className='span-3 img-details'>
-                <h2>NEEDS AN API CALL TO BACKEND(Address)</h2>
-                <p>House For Sale</p>
-                <p className='Price'>NEEDS AN API CALL(price)</p>
-            </div>
-            <div className='info-grid'>
-                <div>
+    return (
+        <div className='featured'>
+            <h1 className='featured-text'>API CALL FOR ADDRESS</h1>
+            <div className='container'>
+                <img className='span-3 image-grid-row-2' src={Apt1} alt='' />
+                <img src={Apt2} alt='' />
+                <img src={Apt3} alt='' />
+                <img src={Apt3} alt='' />
+                <img src={Apt2} alt='' />
+                <div className='img-details span-5 info-grid'>
                     <div className='info'>
-                        <p className='bold'>API CALL(bedrooms)</p>
+                        <p>HOME FOR SALE</p>
                     </div>
                     <div className='info'>
-                        <p className='bold'>API CALL(bathrooms)</p>
-                    </div>
-                </div>
-                <div>
-                    <div className='info'>
-                        <p className='bold'>API CALL(sq feet)</p>
+                        <p>PRICE:700,000</p>
                     </div>
                     <div className='info'>
-                        <p className='bold'>API CALL(estimated pay)</p>
+                        <p>Number of Bedrooms: 5</p>
+                    </div>
+                    <div className='info'>
+                        <p>Number of bathrooms: 7</p>
+                    </div>
+                    <div className='info'>
+                        <p>Square Footage: 7,400ft</p>
+                    </div>
+                    <div className='discription span-5'>
+                        <p>API CALL FOR DESCRIPTION AND INFORMATION: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id leo in velit pretium accumsan vel nec mauris. Suspendisse potenti. Nulla facilisi. Duis interdum eleifend nisl, vel eleifend purus consequat id. Sed auctor ante eu felis condimentum eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; </p>
+                    </div>
+                    <div className='span-5'>
+                        <button className='btn'> View Listing </button>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Featured
